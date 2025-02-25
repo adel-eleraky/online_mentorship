7c3d1d8ada64b2963e93f8c0f9daba5abe1dff74
el3raky <kameladel339@gmail.com>
2025-02-25 21:14:50 +0200

    feat: created room router

---
+++ src/routes/room.router.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import { addMember, createRoom, getRooms, getRoom, deleteMember, getMessagesByRoom } from '../controllers/room.controller.js';
+import { addMemberSchema, createRoomSchema, deleteMemberSchema, validate } from '../middlewares/validation/room.validation.js';
+
+const router = express.Router();
+
+router.get("/", getRooms) // get all room
+router.post("/create", validate(createRoomSchema) ,  createRoom) // create room
+router.get("/:room" , getRoom) // get single room
+router.put("/:room/members" , validate(addMemberSchema) , addMember) // add new member to the room
+router.delete("/:room/members/:member", validate(deleteMemberSchema) , deleteMember) // delete member from the room
+router.get("/:room/messages", getMessagesByRoom) // get all messages in a room
+
+
+export default router
