9568c8d9d7be1f07ff48a88e3b786dd61893c6b9
el3raky <kameladel339@gmail.com>
2025-03-01 21:25:50 +0200

    refactor: added some restriction to delete member from room

--- src/routes/room.router.js
+++ src/routes/room.router.js
@@ -10,8 +10,8 @@ router.use(authMiddleware); // Protect all routes after this middleware
 router.get("/" , restrictTo("admin"), getRooms) // get all room
 router.post("/create", restrictTo("mentor"), validate(createRoomSchema) ,  createRoom) // create room
 router.get("/:room" , getRoom) // get single room
-router.put("/:room/members" , validate(addMemberSchema) , addMember) // add new member to the room
-router.delete("/:room/members/:member", validate(deleteMemberSchema) , deleteMember) // delete member from the room
+// router.put("/:room/members" , validate(addMemberSchema) , addMember) // add new member to the room
+router.delete("/:room/members/:member", restrictTo("admin" , "mentor"), validate(deleteMemberSchema) , deleteMember) // delete member from the room
 router.get("/:room/messages", getMessagesByRoom) // get all messages in a room
 
 
