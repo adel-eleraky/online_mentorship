aea55b013464d6c8235ba4e77f5131a47c04a3e5
el3raky <kameladel339@gmail.com>
2025-03-01 17:30:51 +0200

    test get all messages

--- src/routes/message.router.js
+++ src/routes/message.router.js
@@ -1,11 +1,15 @@
 import express from "express"
-import { sendMessage, deleteMessage } from "../controllers/message.controller.js"
-import { sendMsgSchema, validate } from "../middlewares/validation/message.validation.js"
+// import { sendMessage } from "../controllers/message.controller.js"
+// import { sendMsgSchema, validate } from "../middlewares/validation/message.validation.js"
+import Message from "../models/message.model.js"
 
 const router = express.Router()
 
-
-router.post("/create", validate(sendMsgSchema) , sendMessage ) // create message
-router.delete("/:message", deleteMessage) // delete message
+router.get("/" , async (req, res) => {
+    const data = await Message.find().populate("sender")
+    res.status(200).json({ status: "success", data })
+})
+// router.post("/create", validate(sendMsgSchema) , sendMessage ) // create message
+// router.delete("/:message", deleteMessage) // delete message
 
 export default router
