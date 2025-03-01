d7383a11589c1371827fcd46f927ec51fec1074b
el3raky <kameladel339@gmail.com>
2025-03-01 17:21:18 +0200

    feat: added upload image & update password controllers

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,12 +1,11 @@
-import express from "express";
-import UserModel from "../models/user.model.js"
+import User from "../models/user.model.js"
+import * as bcrypt from 'bcrypt';
 
-const router = express.Router();
 
 // GET all users
 const getAllUsers = async (req, res) => {
   try {
-    const users = await UserModel.find({}, { password: 0, __v: 0 });
+    const users = await User.find({}, { password: 0, __v: 0 });
     res.status(200).json({ success: true, users });
   } catch (err) {
     res.status(500).json({ success: false, message: "Error fetching users", error: err.message });
@@ -15,8 +14,9 @@ const getAllUsers = async (req, res) => {
 
 const getUserById = async (req, res) => {
   try {
-    const { id } = req.user.id;
-    const users = await UserModel.find(id, { password: 0, __v: 0 });
+    const { id } = req.params;
+
+    const users = await User.findById(id, { password: 0, __v: 0 });
     res.status(200).json({ success: true, users });
   } catch (err) {
     res.status(500).json({ success: false, message: "Error fetching users", error: err.message });
@@ -26,7 +26,7 @@ const getUserById = async (req, res) => {
 // Delete all users
 const deleteAllUsers = async (req, res) => {
   try {
-    await UserModel.deleteMany({});
+    await User.deleteMany({});
     res.status(200).json({
       success: true,
       message: "All users deleted successfully",
@@ -44,7 +44,7 @@ const deleteAllUsers = async (req, res) => {
 const deleteUser = async (req, res) => {
   try {
     const { id } = req.params;
-    const user = await UserModel.findByIdAndDelete(id);
+    const user = await User.findByIdAndDelete(id);
 
     if (!user) {
       return res.status(404).json({
@@ -69,18 +69,24 @@ const deleteUser = async (req, res) => {
 
 const updateUser = async (req, res) => {
   try {
-    // const { token } = req.headers;
-    // console.log(token, process.env.TOKEN_SECRET_KEY);
-    // const decode = jwt.verify(token, process.env.TOKEN_SECRET_KEY)
-    // console.log(decode);
 
-    const  {id } = req.user.id;
+    const { id } = req.user;
 
+    const updatedUser = await User.findByIdAndUpdate(id, req.body, { new: true });
 
-    const updatedUser = await userModel.findByIdAndUpdate( id, req.body, { new: true });
-
+    res.status(200).json({
+      success: true,
+      message: "User updated successfully",
+      user: updatedUser
+    });
     // const { email, password, role } = req.body;
-  } catch (err) { }
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: "An error occurred while updating the user",
+      error: err.message,
+    });
+  }
 }
 
 
@@ -88,7 +94,7 @@ const updateUser = async (req, res) => {
 const searchByEmail = async (req, res) => {
   try {
     const { email } = req.params;
-    const user = await UserModel.findOne({ email });
+    const user = await User.findOne({ email });
 
     if (!user) {
       return res.status(404).json({
@@ -110,59 +116,124 @@ const searchByEmail = async (req, res) => {
   }
 };
 
-// Upload profile image
-const updateUserImage = async (req, res) => {
+// // Upload profile image
+// const updateUserImage = async (req, res) => {
+//   try {
+
+//     const id = req.user.id;
+//     console.log('id====================================');
+//     console.log(id);
+//     console.log('====================================');
+
+//     const user = await User.findById(id);
+//     console.log('user====================================');
+//     console.log(user);
+//     console.log('====================================');
+
+//     if (!user) {
+//       return res.status(404).json({
+//         success: false,
+//         message: "User not found",
+//       });
+//     }
+
+//     if (!req.file) {
+//       return res.status(400).json({
+//         success: false,
+//         message: "No file uploaded",
+//       });
+//     }
+
+//     if (user.profileImage === req.file.filename) {
+//       return res.status(400).json({
+//         success: false,
+//         message: "You are trying to upload the same image",
+//       });
+//     }
+
+//     const updatedUser = await User.findByIdAndUpdate(
+//       id,
+//       { profileImage: req.file.filename },
+//       { new: true }
+//     );
+
+//     res.status(200).json({
+//       success: true,
+//       message: "Profile image uploaded successfully",
+//       user: updatedUser,
+//     });
+//   } catch (err) {
+//     res.status(500).json({
+//       success: false,
+//       message: "An error occurred while uploading the profile image",
+//       error: err.message,
+//     });
+//   }
+// };
+
+const getLoggedInUser = async (req, res) => {
+
+  try {
+    const user = await User.findById(req.user.id, { password: 0, __v: 0 });
+    res.status(200).json({ status: "success", data: user });
+  } catch (err) {
+    res.status(500).json({ status: "fail", message: "Error fetching user", error: err.message });
+  }
+}
+
+const uploadProfileImage = async (req, res) => {
   try {
 
-    const id = req.user.id;
-    console.log('id====================================');
-    console.log(id);
-    console.log('====================================');
+    let { id } = req.user
 
-    const user = await UserModel.findById(id);
-    console.log('user====================================');
-    console.log(user);
-    console.log('====================================');
+    const user = await User.findById(id)
+    user.image = req.file.filename
 
-    if (!user) {
-      return res.status(404).json({
-        success: false,
-        message: "User not found",
-      });
-    }
-
-    if (!req.file) {
-      return res.status(400).json({
-        success: false,
-        message: "No file uploaded",
-      });
-    }
-
-    if (user.profileImage === req.file.filename) {
-      return res.status(400).json({
-        success: false,
-        message: "You are trying to upload the same image",
-      });
-    }
-
-    const updatedUser = await UserModel.findByIdAndUpdate(
-      id,
-      { profileImage: req.file.filename },
-      { new: true }
-    );
+    await user.save()
 
     res.status(200).json({
-      success: true,
-      message: "Profile image uploaded successfully",
-      user: updatedUser,
-    });
+      status: "success",
+      message: "Photo uploaded successfully",
+      data: user
+    })
   } catch (err) {
-    res.status(500).json({
-      success: false,
-      message: "An error occurred while uploading the profile image",
-      error: err.message,
-    });
+    res.status(500).json({ message: err.message })
   }
-};
+}
 
-export { getAllUsers, getUserById, deleteAllUsers, deleteUser, updateUser, searchByEmail, updateUserImage };
+
+const updatePassword = async (req, res) => {
+
+  try {
+
+    let { id } = req.user
+    let { newPassword } = req.body
+
+    const password = await bcrypt.hash(newPassword, 8)
+    const user = await User.findByIdAndUpdate(id , {password} , {new: true})
+
+    const objecUser = user.toObject();
+    delete objecUser.password;
+
+    return res.status(200).json({
+      status: "success",
+      message: "Password updated successfully",
+      data: objecUser
+    })
+
+  }catch(err) {
+    return res.status(500).json({status: "fail", message: err.message })
+  }
+}
+
+export {
+  getAllUsers,
+  getUserById,
+  deleteAllUsers,
+  deleteUser,
+  updateUser,
+  searchByEmail,
+  getLoggedInUser,
+  uploadProfileImage,
+  updatePassword
+};
