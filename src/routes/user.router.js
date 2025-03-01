e86466976a5fa19b947d4c5d09a38c2d1ea56a6d
el3raky <kameladel339@gmail.com>
2025-03-01 17:23:04 +0200

    refactor: added validation and restriction to routes

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,23 +1,32 @@
 import { Router } from 'express'
 import * as userService from '../controllers/user.controller.js'
-import {authMiddleware} from '../middlewares/auth/authMiddleware.js';
-// import upload from '../../utils/uploadImage.js';
+import {authMiddleware, restrictTo} from '../middlewares/auth/authMiddleware.js';
+import { passwordSchema, updateSchema, validate } from '../middlewares/validation/user.validation.js';
+import uploadPhoto, { resizePhoto } from '../middlewares/upload.js';
 
 
 const userRouter = Router()
 
+userRouter.use(authMiddleware); // Protect all routes after this middleware
 
-userRouter.get('/users', userService.getAllUsers);
+userRouter.get("/me" , userService.getLoggedInUser); // get current logged-in user
 
-userRouter.get('/user', authMiddleware, userService.getUserById); ////
+userRouter.put('/:id/upload', uploadPhoto , resizePhoto , userService.uploadProfileImage); // upload profile image
 
-userRouter.get("/users/email/:email", userService.searchByEmail);  // Search for users by email
-// userRouter.post("/users/upload", authMiddleware, upload.single("profileImage"), userService.updateUserImage); // Upload profile image
+userRouter.put('/', validate(updateSchema) ,  userService.updateUser); 
 
-userRouter.put('/user', authMiddleware, userService.updateUser); ////
+userRouter.put("/update-password", validate(passwordSchema), userService.updatePassword);
 
-userRouter.delete('/users', userService.deleteAllUsers);
-userRouter.delete('/user/:id', userService.deleteUser);
+userRouter.get('/', restrictTo("admin"), userService.getAllUsers);
+
+userRouter.get('/:id', restrictTo("admin"), userService.getUserById); 
+
+userRouter.get("/search/:email", restrictTo("admin"), userService.searchByEmail);  // Search for users by email
+
+userRouter.delete('/users', restrictTo("admin") , userService.deleteAllUsers);
+
+userRouter.delete('/user/:id', restrictTo("admin"), userService.deleteUser);
+
 
 
 export default userRouter;
