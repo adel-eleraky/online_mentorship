bd3243d651ca50cff168bb839edb1380ba401f24
el3raky <kameladel339@gmail.com>
2025-03-01 17:08:04 +0200

    feat: created session model

---
+++ src/models/session.model.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+
+const sessionSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: [true, "session have a title"],
+            unique: [true, "session title already exists"],
+        },
+        mentor: {
+            type: mongoose.Schema.ObjectId,
+            ref: "Mentor",
+            required: [true, "session must belong to a mentor"],
+        },
+        price: {
+            type: Number,
+            required: [true, "Booking must have a price"],
+        },
+        description: {
+            type: String,
+            required: [true, "session must have a description"],
+        },
+        duration: {
+            type: Number,
+            required: [true, "session must have a duration"],
+        },
+        schedule_time: {
+            type: Date,
+            required: [true, "session must have a schedule time"],
+        },
+        status: {
+            type: String,
+            enum: ["started", "pending", "ended"],
+            default: "pending",
+        },
+        has_room: {
+            type: Boolean,
+            default: false
+        }
+    },
+    { timestamps: true }
+);
+
+
+export default mongoose.model("Session", sessionSchema);
