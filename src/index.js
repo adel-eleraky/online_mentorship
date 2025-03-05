730f50c4e269380689a28cdfa4abe61427a04331
adel eleraky <kameladel339@gmail.com>
2025-03-05 21:14:25 +0200

    feat: imported room handler

--- src/index.js
+++ src/index.js
@@ -7,12 +7,14 @@ import roomRouter from "./routes/room.router.js";
 import sessionRouter from "./routes/session.router.js";
 import messageRouter from "./routes/message.router.js";
 import { Server } from "socket.io";
-import { saveMsg, getRoomMessages } from "./controllers/message.controller.js";
+import { saveMsg, getRoomMessages, messageHandler } from "./controllers/message.controller.js";
 import authRoutes from "./routes/auth.router.js";
 import userRoutes from "./routes/user.router.js";
 import { meetingHandler } from "./controllers/meeting.controller.js";
 import mentorRoutes from "./routes/mentor.router.js";
 import bookingRouter from "./routes/booking.router.js";
+import { roomHandler } from "./controllers/room.controller.js";
+import http from "http"
 
 const app = express();
 
@@ -37,34 +39,39 @@ app.use((err, req, res, next) => {
   return res.status(409).json({
     status: "fail",
     message: err.message,
+    error: err
   });
 });
 
 mongoose.connect("mongodb://127.0.0.1:27017/mentorship").then((conn) => {
   console.log("DB connected Successfully");
 
+  const server = http.createServer(app)
+
   const port = process.env.PORT || 3000;
-  const server = app.listen(port, () => {
-    console.log(`App listening on port ${port}`);
+  server.listen(port, () => {
+    console.log(`server listening on port ${port}`);
 
     const io = new Server(server, {
       cors: {
-        origin: "*",
-        credentials: true,
+        origin: "http://localhost:5173",
+        methods: ["GET", "POST"]
       },
     });
 
     io.on("connection", (socket) => {
-      socket.on("join_room", async (id) => {
-        // get all messages in the room
-        socket.emit("send_room_messages", await getRoomMessages(id));
-      });
+      // socket.on("join_room", async (id) => {
+      //   // get all messages in the room
+      //   socket.emit("send_room_messages", await getRoomMessages(id));
+      // });
       // console.log("New connection" , socket.id)
-      socket.on("send_message", async (data) => {
-        // console.log(data)
-        await saveMsg(data);
-        io.emit("receive_message", data);
-      });
+      // socket.on("send_message", async (data) => {
+      //   // console.log(data)
+      //   await saveMsg(data);
+      //   io.emit("receive_message", data);
+      // });
+      // messageHandler(socket)
+      roomHandler(socket, io)
       meetingHandler(socket);
     });
   });
