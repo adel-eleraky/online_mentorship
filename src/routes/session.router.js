3fd189661d2c7676e7a5b25eedf1a1ecdc040ccd
ahmedrashad80 <ahmedmohamedrashad80@gmail.com>
2025-03-16 23:19:48 +0200

    end point to get video token

--- src/routes/session.router.js
+++ src/routes/session.router.js
@@ -1,10 +1,10 @@
 import express from "express"
 import { authMiddleware, restrictTo } from "../middlewares/auth/authMiddleware.js"
-import { createSession } from "../controllers/session.controller.js"
+import { createSession ,getVideoToken} from "../controllers/session.controller.js"
 import { createSessionSchema, validate } from "../middlewares/validation/session.validation.js"
 
 const router = express.Router()
 
 router.post("/" ,authMiddleware, restrictTo("mentor") , validate(createSessionSchema), createSession) // create session
-
+router.get("/getVideoToken",getVideoToken)
 export default router
