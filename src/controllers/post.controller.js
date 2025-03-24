2cd596e827a12793b57c11f667c7ba01e9922545
adel eleraky <kameladel339@gmail.com>
2025-03-24 04:32:22 +0200

    feat: created posts controller ( get all & create & delete )

---
+++ src/controllers/post.controller.js
@@ -0,0 +1,108 @@
+import Post from "../models/post.model.js"
+
+export const getAllPosts = async (req, res) => {
+    try {
+
+        const posts = await Post.find().populate("user")
+
+        return res.status(200).json({
+            status: "success",
+            message: "Posts fetched successfully",
+            data: posts
+        })
+    } catch (err) {
+
+        res.status(500).json({
+            status: "fail",
+            message: "Internal server error",
+            error: err.message
+        });
+    }
+}
+
+export const createPost = async (req, res) => {
+    try {
+
+        const post = await Post.create({ ...req.body, user: req.user.id, user_role: req.user.role })
+
+        return res.status(200).json({
+            status: "success",
+            message: "Post created successfully",
+            data: post
+        })
+    } catch (err) {
+
+        return res.status(500).json({
+            status: "fail",
+            message: "Internal server error",
+            error: err.message
+        });
+    }
+}
+
+
+// user delete his post
+export const deletePost = async (req, res) => {
+    try {
+
+        const post = await Post.findOne({_id: req.params.id , user: req.user.id})
+
+        await post.deleteOne()
+
+        return res.status(200).json({
+            status: "success",
+            message: "Post deleted successfully",
+        })
+    } catch (err) {
+
+        return res.status(500).json({
+            status: "fail",
+            message: "Internal server error",
+            error: err.message
+        });
+    }
+}
+
+// get logged-in user post
+export const getUserPosts = async (req, res) => {
+
+    try {
+
+        const posts = await Post.find({user: req.user.id})
+
+        return res.status(200).json({
+            status: "success",
+            message: "Posts fetched successfully",
+            data: posts
+        })
+    } catch (err) {
+
+        res.status(500).json({
+            status: "fail",
+            message: "Internal server error",
+            error: err.message
+        });
+    }
+}
+
+// get specific user posts
+export const getPostsByUserId  = async (req, res) => {
+
+    try {
+
+        const posts = await Post.find({user: req.params.id})
+
+        return res.status(200).json({
+            status: "success",
+            message: "Posts fetched successfully",
+            data: posts
+        })
+    } catch (err) {
+
+        res.status(500).json({
+            status: "fail",
+            message: "Internal server error",
+            error: err.message
+        });
+    }
+}
