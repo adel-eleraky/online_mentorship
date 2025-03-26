9285741bbf808b971df44fe787323b3437f85342
adel eleraky <kameladel339@gmail.com>
2025-03-26 17:46:16 +0200

    refactor: modified likes schema

--- src/models/like.model.js
+++ src/models/like.model.js
@@ -1,24 +1,27 @@
-
 import mongoose from "mongoose"
 
 const likesSchema = new mongoose.Schema({
-
-    user: {
+    
+    post: {
         type: mongoose.Schema.Types.ObjectId,
-        refPath: "user_role",
         required: true
     },
-    user_role: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        enum: ["User" , "Mentor"]
-    },
-    like_type: {
-        type: String,
-        required: [true , "Like type is required"]
-    }
+    likes: [
+        {
+            user: {
+                type: mongoose.Schema.Types.ObjectId,
+                refPath: "likes.role",
+                required: true
+            },
+            role: {
+                type: String,
+                enum: ["User", "Mentor"],
+                required: true
+            }
+        }
+    ],
 })
 
-const likesModel = mongoose.model("Like" , likesSchema)
+const likesModel = mongoose.model("Like", likesSchema)
 
 export default likesModel
