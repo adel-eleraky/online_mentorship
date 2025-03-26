8536d86919830160b56633110454a873293dd1cd
adel eleraky <kameladel339@gmail.com>
2025-03-26 17:46:54 +0200

    feat: create routes for get all post & get posts for user

--- src/routes/post.router.js
+++ src/routes/post.router.js
@@ -1,13 +1,14 @@
 import express from "express"
-import { getAllPosts, createPost, getUserPosts, deletePost } from "../controllers/post.controller.js"
+import { getAllPosts, createPost, getUserPosts, deletePost, getPostsByUserId } from "../controllers/post.controller.js"
 import { createPostSchema, validate } from "../middlewares/validation/post.validation.js"
 import { authMiddleware } from "../middlewares/auth/authMiddleware.js"
 
 const postRouter = express.Router()
 
 
-postRouter.get("/" , getAllPosts)
+postRouter.get("/" ,authMiddleware , getAllPosts)
 postRouter.post("/" , authMiddleware , validate(createPostSchema), createPost)
 postRouter.delete("/:id" , authMiddleware , deletePost)
+postRouter.get("/user/:id" , authMiddleware , getPostsByUserId)
 
 export default postRouter
