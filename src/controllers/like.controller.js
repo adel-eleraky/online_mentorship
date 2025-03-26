bf4670d76aa2b57445315e4aa4f54bc4b792a281
adel eleraky <kameladel339@gmail.com>
2025-03-26 17:45:29 +0200

    feat: created likes controller ( create like & get post likes )

---
+++ src/controllers/like.controller.js
@@ -0,0 +1,58 @@
+import Like from "../models/like.model.js"
+
+export const createLike = async (req, res) => {
+
+    try {
+
+        const { id, role } = req.user
+        const { post } = req.body
+
+        var newLike;
+        const existPost = await Like.findOne({ post })
+        if (!existPost) {
+            newLike = await Like.create({ post, likes: [ { user: id, role } ] })
+        } else {
+            newLike = await Like.findOneAndUpdate(
+                { post },
+                { $push: { likes: {user: id , role }}},
+                { new: true }
+            )
+        }
+
+        return res.status(200).json({
+            status: "success",
+            message: "like added successfully",
+            data: newLike
+        })
+    } catch (err) {
+        return res.status(500).json({
+            status: "fail",
+            message: "Internal server error",
+            error: err.message
+        });
+    }
+}
+
+export const getPostLikes = async (req, res) =>{
+    try {
+
+        const { id: post } = req.params
+        
+        const likes = await Like.find({ post }).populate({ 
+            path: "likes.user",
+            select: "name"
+        })
+
+        return res.status(200).json({
+            status: "success",
+            message: "like fetched successfully",
+            data: likes
+        })
+    } catch (err) {
+        return res.status(500).json({
+            status: "fail",
+            message: "Internal server error",
+            error: err.message
+        });
+    }
+}
