2054a5ac075bdc86e679238dcc6219c27a1d2a3b
adel eleraky <kameladel339@gmail.com>
2025-04-09 03:43:04 +0200

    feat: uploaded image for post

--- src/routes/post.router.js
+++ src/routes/post.router.js
@@ -2,12 +2,13 @@ import express from "express"
 import { getAllPosts, createPost, getUserPosts, deletePost, getPostsByUserId } from "../controllers/post.controller.js"
 import { createPostSchema, validate } from "../middlewares/validation/post.validation.js"
 import { authMiddleware } from "../middlewares/auth/authMiddleware.js"
+import uploadPhoto, { resizePhoto } from "../middlewares/upload.js"
 
 const postRouter = express.Router()
 
 
 postRouter.get("/" ,authMiddleware , getAllPosts)
-postRouter.post("/" , authMiddleware , validate(createPostSchema), createPost)
+postRouter.post("/" , authMiddleware ,  uploadPhoto , resizePhoto , validate(createPostSchema), createPost)
 postRouter.delete("/:id" , authMiddleware , deletePost)
 postRouter.get("/user/:id" , authMiddleware , getPostsByUserId)
 
